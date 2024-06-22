1083419825881a611200ae6791911bb8ee8a8c76
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2024-06-22 12:14:05 -0700

    Server unity continue

--- CouchCampaign/Assets/Scripts/ServerConnection.cs
+++ CouchCampaign/Assets/Scripts/ServerConnection.cs
@@ -1,7 +1,5 @@
 using MikeSchweitzer.WebSocket;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 public class ServerConnection : MonoBehaviour
 {
@@ -9,8 +7,45 @@ public class ServerConnection : MonoBehaviour
 
     private void Start()
     {
-        websocket = new WebSocketConnection();
-        websocket.Connect("wss://ws.postman-echo.com/raw");
+        websocket.StateChanged += OnStateChanged;
+        websocket.Connect("ws://localhost:8000/ws/unity");
     }
 
+    private void OnDestroy()
+    {
+        websocket.StateChanged -= OnStateChanged;
+        websocket.Disconnect();
+    }
+
+    void Update()
+    {
+        while (websocket.TryRemoveIncomingMessage(out string message))
+        {
+            var json = JsonUtility.FromJson<JsonData>(message);
+
+            //Do something here...
+        }
+    }
+
+
+
+    private void OnStateChanged(WebSocketConnection connection, WebSocketState oldState, WebSocketState newState)
+    {
+
+        if (newState == WebSocketState.Connected)
+        {
+            Debug.Log("Server Connected");
+        }
+        else if (newState == WebSocketState.Disconnected)
+        {
+            Debug.Log("Server Disconnected");
+            websocket.Connect();
+        }
+    }
+
+    [Serializable]
+    private class JsonData
+    {
+        public string type;
+    }
 }
