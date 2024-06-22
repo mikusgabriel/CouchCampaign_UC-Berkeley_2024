108e0dff04e22b581f378e8f322bd4ab93d01014
Laurent Stéphenne <laurent@guibi.dev>
2024-06-22 13:33:22 -0700

    Small incomplete ws implementation

--- Server/main.py
+++ Server/main.py
@@ -6,11 +6,12 @@ from fastapi import FastAPI, Response, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 
 import voiceApi
-import meshyApi
 from connections import Connections
+from game import GameManager
 
 app = FastAPI()
 connections = Connections()
+gameManager = GameManager()
 
 
 class LoginInfo(BaseModel):
@@ -22,7 +23,14 @@ async def login_POST(info: LoginInfo, res: Response):
     userId = info.name
     res.set_cookie("id", userId)
 
-    return {"success": True, "id": userId}
+    if gameManager.getPlayer(userId) is None:
+        return {"success": True, "id": userId, "state": "create"}
+    return {
+        "success": True,
+        "id": userId,
+        "state": "wait",
+        "user": gameManager.getPlayer(userId).toPOJO(),
+    }
 
 
 @app.post("/logout")
@@ -34,24 +42,59 @@ async def logout_POST(res: Response):
 @app.websocket("/ws")
 async def ws_WEBSOCKET(ws: WebSocket):
     userId = ws.cookies.get("id")
-    if userId is None:
-        return {"success": False, "message": "Missing user id. Please login in first."}
+    if (userId is None) or (gameManager.getPlayer(userId) is None):
+        return {"success": False, "message": "Please login in first."}
 
     await ws.accept()
     connections.add_client(userId, ws)
+
+    if gameManager.getPlayer(userId) is None:
+        await connections.send_client(userId, {"type": "user", "state": "create"})
 
     try:
         while True:
             data = json.loads(await ws.receive_text())
             print("User data:", userId, data)
 
-            recordingAudioData = base64.b64decode(data["recording"].split(",")[1])
-            [transcript, emotions] = asyncio.gather(
-                voiceApi.getVoiceTranscript(recordingAudioData),
-                voiceApi.getVoiceEmotions(recordingAudioData),
-            )
+            match data["type"]:
+                case "end-turn":
+                    if gameManager.currentTurn.player.id() != userId:
+                        continue
+                    await gameManager.nextTurn()
 
-            connections.send_client(userId, {"type": "transcript", "data": transcript})
+                case "voice":
+                    if gameManager.currentTurn.player.id() != userId:
+                        continue
+
+                    encodedAudioData = data["recording"].split(",")[1]
+                    audioData = base64.b64decode(encodedAudioData)
+                    [transcript, emotions] = await asyncio.gather(
+                        voiceApi.getVoiceTranscript(audioData),
+                        voiceApi.getVoiceEmotions(audioData),
+                    )
+
+                    await ws.send_json({"type": "transcript", "data": transcript})
+
+                case "create":
+                    if gameManager.currentTurn is not None:
+                        await ws.send_json(
+                            {
+                                "type": "error",
+                                "message": "A game is already in progress",
+                            }
+                        )
+                        continue
+
+                    await gameManager.createPlayer(
+                        userId, data["race"], data["classe"], data["description"]
+                    )
+
+                case "move":
+                    if gameManager.currentTurn.player.id() != userId:
+                        continue
+
+                    await gameManager.movePlayer(userId, data["x"], data["y"])
+                    await gameManager.nextTurn()
 
     except WebSocketDisconnect:
         connections.remove_client(userId)
@@ -66,8 +109,7 @@ async def ws_WEBSOCKET_UNITY(ws: WebSocket):
     try:
         while True:
             data = json.dumps(await ws.receive_text())
-            print("received", data)
+            print("Unity WebSocket received:", data)
 
     except WebSocketDisconnect:
         connections.set_unity(None)
-        print("websocket disconnected")
--- Server/meshyApi.py
+++ Server/meshyApi.py
@@ -4,13 +4,13 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 
-def generateMeshyMesh(promptInput):
+def generateMeshyMesh(race: str, classe: str, decription: str):
     response = requests.post(
         "https://api.meshy.ai/v2/text-to-3d",
         headers={"Authorization": f"Bearer {config["meshyKey"]}"},
         json={
             "mode": "preview",
-            "prompt": f"{promptInput}",
+            "prompt": f"A {classe} {race} character in a dnd game. {decription}",
             "art_style": "realistic",
             "negative_prompt": "low quality, low resolution, low poly, ugly, has a base, has no feet",
         },
