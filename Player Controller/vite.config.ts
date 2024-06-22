50f1262406d0ae56693190e5b579d61604964b69
Laurent Stéphenne <laurent@guibi.dev>
2024-06-22 13:46:40 -0700

    Initial setup

--- Player Controller/vite.config.ts
+++ Player Controller/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api/ws": {
+                target: "ws://localhost:8000",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+                ws: true,
+            },
+            "/api": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+    resolve: {
+        alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+    },
+});
